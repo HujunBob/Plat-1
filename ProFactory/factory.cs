bdb774246d856ba315212f51f506727155be2f1c
buffer <1045931706@qq.com>
2017-02-18 10:26:05 +0800

    git initial 1

---
+++ ProFactory/factory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProOperator;
+using System.IO;
+using System.Reflection;
+
+
+namespace ProFactory
+{
+    public class factory
+    {
+        public static Operation GetOpre(string type)
+        {
+            Operation oper = null;
+            //获得debug下plus文件夹所有的路径
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dll");
+            string[] files = Directory.GetFiles(path, "*.dll");
+            foreach (string file in files)
+            {
+                Assembly ass = Assembly.LoadFile(file);
+                //获得程序集中所有元数据
+                Type[] types = ass.GetExportedTypes();
+                foreach (Type tt in types)
+                {
+                    if (tt.IsSubclassOf(typeof(Operation)))
+                    {
+                        oper = (Operation)Activator.CreateInstance(tt);
+                    }
+                    if (type == oper.type)
+                    {
+                        return oper;
+                    }
+                    else
+                    {
+                        oper = null;
+                    }
+                }
+            }
+            return oper;
+        }
+
+    }
+}
---
+++ ProOperator/Operation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProOperator
+{
+    public abstract class Operation
+    {
+        //public OutData data;
+        //public IuputData data_;
+
+        public List<int> result;
+        public abstract string type { get; }
+        public abstract int[] output();
+
+        //一些基础参数
+        public const double K = 7.5;       //K值
+        public const int maxshuiwei = 157;
+        public const int minshuiwei = 139;
+        public const double maxchuli = 900;
+        public const double minchuli = 30;
+
+        //public double[,] laishui = new double[71, 12];
+
+
+        public List<double> shuiwei = new List<double>();
+        public List<double> kurong = new List<double>();
+        public List<double> xiayoushuiwei = new List<double>();
+        public List<double> xiaxieliuliang = new List<double>();
+        public double[,] laishui = new double[71, 12];
+
+        //public List<List<double>> laishui = new List<List<double>>();
+        public int shijian;
+        public int shujugeshu;
+        public int maxchukuliuliang;
+        public int minchukuliuliang;
+        public int maxshuilunji;
+        public double N1;
+        public double N2;
+
+        //output
+        public double[] chuli = new double[12];
+        public double[] qishui1 = new double[12];
+        public double[] route1 = new double[13];
+        public double NNmax;
+        public double[] chuku = new double[12];
+        public string[] V = new string[12];
+
+        //抽象方法，需要在子类中重写
+        public abstract void input(List<double> Basic_shuiwei, List<double> Basic_kurong, List<double> Basic_xiayoushuiwei, List<double> Basic_xiaxieliuliang, double[,] Basic_laishui,
+           int Basic_shijian, int Basic_shujugeshu, int Basic_maxchukuliuliang, int Basic_minchukuliuliang, int Basic_maxshuilunji, double Basic_N1, double Basic_N2);
+
+
+    }
+}
